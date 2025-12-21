4186840b7b2f9dd6881b1a9ba262f535fd2ca1f2
Jeyaseelan R <jeyaseelan.rajendran@handbuiltapps.com>
2025-12-21 15:13:59 +0530

    Updates

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,16 +1,47 @@
 import React from 'react';
-import { Text } from '../../components';
+import { Center, Text } from '../../components';
+import { login_img } from '../../components/Images';
+
+import style from './style.module.css';
 
 const Login = () => {
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+    
+  }
+
+
   return (
-    <div className="card">
-      <img src="..." className="card-img-top" alt="..." />
-      <div className="card-body">
-        <Text className="card-title h1 text-center fs-1">Login</Text>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-        <a href="#" className="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
+    <Center>
+      <form className={style.loginWrapper} onSubmit={onSubmit}>
+        <div className="card w-50">
+          <div className='pt-3 px-3 text-center'>
+            <img src={login_img} className={`card-img-top ${style.image}`} alt="Login" />
+          </div>
+          <div className="card-body">
+            <Text className="card-title h1 text-center fs-1" breeSerif>Login</Text>
+            <div className="mb-3">
+              <label htmlFor="loginExampleFormControlInput1" className="form-label">Email address</label>
+              <input type="email" className="form-control" id="loginExampleFormControlInput1" placeholder="name@example.com" />
+            </div>
+
+            <div className="mb-3">
+              <label htmlFor="loginInputPassword5" className="form-label">Password</label>
+              <input type="password" id="loginInputPassword5" className="form-control" aria-describedby="loginPasswordHelpBlock" />
+              <div id="loginPasswordHelpBlock" className="form-text">
+                Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
+              </div>
+            </div>
+
+            <div className='text-center'>
+              <button type='submit' className="btn btn-primary text-uppercase">Submit</button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </Center>
   )
 }
 
---
+++ src/components/Images.js
@@ -0,0 +1,5 @@
+import login_img from "../assets/login.png";
+
+export {
+    login_img
+}
