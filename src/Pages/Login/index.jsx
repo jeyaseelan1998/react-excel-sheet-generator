54513d8eca1ae0331b9d2a0ad2ca582762d01bfc
Jeyaseelan R <jeyaseelan.rajendran@handbuiltapps.com>
2025-12-22 23:51:28 +0530

    add api

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,51 +1,61 @@
 import React from 'react';
 import { Center, Text } from '../../components';
 import { login_img } from '../../components/Images';
-import { setCookie } from '../../utils/storage';
 
 import style from './style.module.css';
 import MetaTag from '../../components/MetaTag';
+import { Field, Form } from 'react-final-form';
+import { required } from '../../utils/validate';
+import api from '../../api/v1';
 
 const Login = () => {
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-    console.log(e.target);
-    setCookie('2d3d333dasda23dd33d3d');
+  const onSubmit = async (data) => {
+    await api.post("/login", data);
     window.location.href = '/';
   }
 
   return (
     <>
-    <MetaTag title="Login" />
-    <Center>
-      <form className={style.loginWrapper} onSubmit={onSubmit}>
-        <div className="card">
-          <div className='pt-3 px-3 text-center'>
-            <img src={login_img} className={`card-img-top ${style.image}`} alt="Login" />
-          </div>
-          <div className="card-body">
-            <Text className="card-title h1 text-center fs-1" breeSerif>Login</Text>
-            <div className="mb-3">
-              <label htmlFor="loginExampleFormControlInput1" className="form-label">Email address</label>
-              <input required type="email" className="form-control" id="loginExampleFormControlInput1" placeholder="name@example.com" />
-            </div>
+      <MetaTag title="Login" />
+      <Center>
+        <Form
+          initialValues={{
+            email: "test@test.com",
+            password: "Test2025"
+          }}
+          onSubmit={onSubmit}
+          render={({ handleSubmit, valid }) => {
+            return (
+              <form className={style.loginWrapper} onSubmit={handleSubmit}>
+                <div className="card">
+                  <div className='pt-3 px-3 text-center'>
+                    <img src={login_img} className={`card-img-top ${style.image}`} alt="Login" />
+                  </div>
+                  <div className="card-body">
+                    <Text className="card-title h1 text-center fs-1" breeSerif>Login</Text>
+                    <div className="mb-3">
+                      <label htmlFor="loginExampleFormControlInput1" className="form-label">Email address</label>
+                      <Field name="email" component="input" type='email' className="form-control" id="loginExampleFormControlInput1" required placeholder="name@example.com" validate={required} />
+                    </div>
+                    <div className="mb-3">
+                      <label htmlFor="loginInputPassword5" className="form-label">Password</label>
+                      <Field name="password" component="input" type='password' className="form-control" id="loginInputPassword5" required aria-describedby="loginPasswordHelpBlock" validate={required} />
+                      <div id="loginPasswordHelpBlock" className="form-text">
+                        Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
+                      </div>
+                    </div>
 
-            <div className="mb-3">
-              <label htmlFor="loginInputPassword5" className="form-label">Password</label>
-              <input required type="password" id="loginInputPassword5" className="form-control" aria-describedby="loginPasswordHelpBlock" />
-              <div id="loginPasswordHelpBlock" className="form-text">
-                Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
-              </div>
-            </div>
-
-            <div className='text-center'>
-              <button type='submit' className="btn btn-primary text-uppercase">Submit</button>
-            </div>
-          </div>
-        </div>
-      </form>
-    </Center>
+                    <div className='text-center'>
+                      <button type='submit' className="btn btn-primary text-uppercase" disabled={!valid}>Submit</button>
+                    </div>
+                  </div>
+                </div>
+              </form>
+            )
+          }}
+        />
+      </Center>
     </>
   )
 }
---
+++ src/api/v1.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+var api = axios.create({
+    baseURL: import.meta.env.VITE_APP_API_URL,
+    withCredentials: true,
+});
+
+api.interceptors.request.use((config) => {
+    let conf = {
+        ...config,
+        headers: {
+            ...config.headers,
+            'Content-Type': config.headers['Content-Type'] ? config.headers['Content-Type'] : 'application/json',
+        }
+    }
+    return conf;
+})
+
+api.interceptors.response.use(response => {
+    return response;
+}, (error) => {
+    return Promise.reject(error);
+})
+
+export default api;
