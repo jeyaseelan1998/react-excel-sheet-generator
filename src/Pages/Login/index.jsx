3b6e8cd74b6bd9492f87eb21cd3054f595567b6e
Jeyaseelan R <jeyaseelan.rajendran@handbuiltapps.com>
2025-12-23 10:27:35 +0530

    add login spinner

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,7 +1,7 @@
 import { get } from 'lodash';
 import { toast } from 'react-toastify';
 import React, { useState } from 'react';
-import { Center, Text } from '../../components';
+import { Center, Spinner, Text } from '../../components';
 import { login_img } from '../../components/Images';
 
 import api from '../../api/v1';
@@ -47,18 +47,31 @@ const Login = () => {
                     <Text className="card-title h1 text-center fs-1" breeSerif>Login</Text>
                     <div className="mb-3">
                       <label htmlFor="loginExampleFormControlInput1" className="form-label">Email address</label>
-                      <Field name="email" component="input" type='email' className="form-control" id="loginExampleFormControlInput1" required placeholder="name@example.com" validate={required} />
+                      <Field name="email" component="input" type='email' className="form-control" id="loginExampleFormControlInput1" required placeholder="name@example.com" validate={required} autoComplete="username" />
                     </div>
                     <div className="mb-3">
                       <label htmlFor="loginInputPassword5" className="form-label">Password</label>
-                      <Field name="password" component="input" type='password' className="form-control" id="loginInputPassword5" required aria-describedby="loginPasswordHelpBlock" validate={required} />
-                      <div id="loginPasswordHelpBlock" className="form-text">
+                      <Field name="password" component="input" type='password' className="form-control" id="loginInputPassword5" required aria-describedby="loginPasswordHelpBlock" validate={required} autoComplete="current-password" />
+                      {/* <div id="loginPasswordHelpBlock" className="form-text">
                         Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
-                      </div>
+                      </div> */}
                     </div>
 
-                    <div className='text-center'>
-                      <button type='submit' className="btn btn-primary text-uppercase w-25" disabled={!valid || fetching}>Submit</button>
+                    <div className='text-center pt-3'>
+                      {
+                        !fetching && (
+                          <button type='submit' className="btn btn-primary text-uppercase w-25" disabled={!valid}>
+                            Submit
+                          </button>
+                        )
+                      }
+                      {
+                        fetching && (
+                          <div className="btn btn-primary opacity-50 text-uppercase w-25 m-auto d-flex justify-content-center">
+                            <Spinner />
+                          </div>
+                        )
+                      }
                     </div>
                   </div>
                 </div>
